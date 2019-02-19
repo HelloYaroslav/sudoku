ce7e8136b3838dcac497e98d79648a666df488ca
Your Name <you@example.com>
2019-02-19 18:39:05 +0300

    resolved

--- src/index.js
+++ src/index.js
@@ -1,3 +1,71 @@
 module.exports = function solveSudoku(matrix) {
-  // your solution
+  let solv_arr = matrix;
+  if (solved(matrix)) {
+      solv_arr = matrix;
+  }
+
+  return solv_arr;
+}
+
+function solved(arr) {
+  let l = [0, 0];
+
+  if (empty_loc(arr, l)) return true;
+  let row = l[0];
+  let col = l[1];
+
+  for (let num = 1; num < 10; num++) {
+      if (loc_is_ok(arr, row, col, num)) {
+          arr[row][col] = num;
+          if (solved(arr)) return true;
+          arr[row][col] = 0;
+      }
+  }
+  return false;
+}
+
+function loc_is_ok(arr, row, col, num) {
+  return in_row(arr, row, num) && in_col(arr, col, num) && in_box(arr, row - row % 3, col - col % 3, num);
+}
+
+function empty_loc(arr, l) {
+  for (let row = 0; row < 9; row++) {
+      for (let col = 0; col < 9; col++) {
+          if (arr[row][col] === 0) {
+              l[0] = row;
+              l[1] = col;
+              return false;
+          }
+      }
+  }
+  return true;
+}
+
+function in_row(arr, row, num) {
+  for (let i = 0; i < 9; i++) {
+      if (arr[row][i] === num) {
+          return false;
+      }
+  }
+  return true;
+}
+
+function in_col(arr, col, num) {
+  for (let y = 0; y < 9; y++) {
+      if (arr[y][col] === num) {
+          return false;
+      }
+  }
+  return true;
+}
+
+function in_box(arr, row, col, num) {
+  for (let i = 0; i < 3; i++) {
+      for (let y = 0; y < 3; y++) {
+          if (arr[i + row][y + col] === num) {
+              return false;
+          }
+      }
+  }
+  return true;
 }
